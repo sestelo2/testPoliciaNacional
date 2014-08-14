c10978cf9cbbce1488dd8c5aa1ab717a46dcfb7f
sestelo <diego.sestelo@gmail.com>
2014-08-14 20:04:10 +0200

    Pantallas Diego
    Pantalla de registro y botones de inicio

---
+++ app/src/main/java/com/example/sestelo/testpolicianacional/login.java
@@ -0,0 +1,60 @@
+package com.example.sestelo.testpolicianacional;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.sestelo.testpolicianacional.R;
+
+public class login extends Activity {
+
+    private Button botonLogin;
+    private Button botonRegistrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        botonRegistrar = (Button) findViewById(R.id.botonRegistrarse);
+        botonRegistrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                Intent myIntent = new Intent(v.getContext(), registro.class);
+                startActivity(myIntent);
+                
+            }
+        });
+
+        botonLogin = (Button) findViewById(R.id.botonEntrar);
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                Intent myIntent = new Intent(v.getContext(), inicio.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
